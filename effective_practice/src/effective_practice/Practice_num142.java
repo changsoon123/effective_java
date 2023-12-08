bcc24dcc0a99e6526074179cb8ef635254609b9c
changsoon123 <sool0458@naver.com>
2023-12-08 11:45:28 +0900

    annotation practice

---
+++ effective_practice/src/effective_practice/Practice_num142.java
@@ -0,0 +1,49 @@
+package effective_practice;
+
+import java.math.BigInteger;
+
+public class Practice_num142<E> {
+
+	 /**
+     * Returns the element at the specified position in this list.  
+     *
+     * <p>This method is <i>not</i> guaranteed to run in constant
+     * time. In some implementations it may run in time proportional
+     * to the element position.
+     *
+     * @param  index index of element to return; must be
+     *         non-negative and less than the size of this list
+     * @return the element at the specified position in this list
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *         ({@code index < 0 || index >= this.size()})
+     */
+    E get(int index) {
+        return null;
+    }
+	
+    /**
+     * Returns true if this collection is empty. 
+     *
+     * @implSpec This implementation returns {@code this.size() == 0}.
+     *
+     * @return true if this collection is empty
+     */
+    public boolean isEmpty() {
+        return false;
+    }
+    
+    /**
+     * A suspect, such as Colonel Mustard or {@literal Mrs. Peacock}.
+     */
+      public enum Suspect {}
+    
+      /**
+       *{@summary A suspect, such as Colonel Mustard or Mrs. Peacock.}
+       */
+       public enum Suspect2 {}
+      
+	public static void main(String[] args) {
+
+	}
+
+}
