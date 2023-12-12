321926a2c5004d75e6bf26d7360dfc6acd4ad469
changsoon123 <sool0458@naver.com>
2023-12-12 17:29:40 +0900

    local variable practice

---
+++ effective_practice/src/effective_practice/Practice_num144.java
@@ -0,0 +1,62 @@
+package effective_practice;
+
+import java.util.Iterator;
+
+import javax.lang.model.element.Element;
+
+public class Practice_num144 {
+
+	public static void main(String[] args) {
+
+		
+		
+		Element[] c = null;
+		
+		for (Element e : c){
+			
+		}
+	
+		
+		Iterable<Element> d = null;
+		
+		
+		for (Iterator<Element> i = d.iterator(); i.hasNext(); ) { 
+			
+			Element e = i.next();
+		}
+		
+		for (Iterator<Element> i = d.iterator(); i.hasNext(); ) { 
+			
+			Element e = i.next();
+		}
+		
+		for (Iterator<Element> i2 = d2.iterator(); i.hasNext(); ) { 
+			
+			Element e = i2.next();
+		}
+		
+		for(int i=0, n= expensiveComputation(); i< n; i++) {
+			
+		}
+		
+		Iterator<Element> i = d.iterator();
+		while(i.hasNext()) {
+			doSomething(i.next());
+		}
+			
+		Iterator<Element> i2 = d.iterator();
+		while(i.hasNext()) {  
+			doSomething(i2.next());
+		}
+		
+	}
+
+	private static int expensiveComputation() {
+		return 0;
+	}
+
+	private static void doSomething(Element next) {
+		
+	}
+
+}
