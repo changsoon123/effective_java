8662a2dcd9dc7c5becfd0710b7e7a1b9a0427cef
changsoon123 <sool0458@naver.com>
2023-12-20 17:36:29 +0900

    interface and class practice

---
+++ effective_practice/src/effective_practice/Practice_num152.java
@@ -0,0 +1,21 @@
+package effective_practice;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Practice_num152 {
+
+	public static void main(String[] args) {
+
+		Set<Son> sonSet = new HashSet<>();
+		
+		LinkedHashSet<Son> sonSet2 = new LinkedHashSet<>();
+		
+	}
+
+}
+
+class Son {
+	
+}
